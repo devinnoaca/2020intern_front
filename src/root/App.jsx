7f9bb43d6c8a537197eefff022273e7df163d297
Jihun <wlgnssla311@gmail.com>
2020-07-28 17:27:41 +0900

    UserContext 추가

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,32 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom'
 
 import 'style/App.css';
-import Api from 'api/Api';
 import { Main, Intro, MyPage } from 'pages';
+import UserProvider from 'provider/UserProvider';
 
 const App = () => {
-  const greeting = async () => {
-    await Api.greeting()
-      .then((res) => {
-        console.log(res.data);
-      });
-  };
-
-  greeting();
-
-  useEffect(() => {
-    console.log('hi');
-  });
-
   return (
     <div className="App">
-      <Route exact path='/' component={Intro} />
-      <Route exact path='/main' component={Main} />
-      <Route exact path='/mypage' component={MyPage} />
+      <UserProvider>
+        <Route exact path='/' component={Intro} />
+        <Route exact path='/main' component={Main} />
+        <Route exact path='/mypage' component={MyPage} />
+      </UserProvider>
     </div>
   );
-
 };
 
 export default App;
