a99dbaa29f523c1eb67c014afd99289ba8b10a39
showppark12 <showppark12@naver.com>
2020-08-04 09:15:24 +0900

    멘토리스트 모달 디테일 페이지 만듬

--- src/components/main/MentorListB.jsx
+++ src/components/main/MentorListB.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 
 import 'style/Main.css';
 import MentorListContext from 'context/MentorListContext';
+import image from 'style/logo192.png';
 
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
@@ -13,22 +14,39 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
     root: {
         maxWidth: 345,
     },
     media: {
-        height: 140,
+        height: 300,
     },
 });
 
 const MentorListB = () => {
     const classes = useStyles();
+    const [pickedMentor, setPickedMentor] = useState({
+        usn: '',
+        name: '',
+        imageUrl: '',
+        email: '',
+        password: '',
+        description: '',
+        company: '',
+        career: [],
+    });
     const { mentorList } = useContext(MentorListContext);
     const [modalShow, setModalShow] = React.useState(false);
 
     function MyVerticallyCenteredModal(props) {
+        // const [value, setValue] = React.useState('Controlled');
+
+        // const handleChange = (event) => {
+        //     setValue(event.target.value);
+        // };
         return (
             <Modal
                 {...props}
@@ -37,16 +55,45 @@ const MentorListB = () => {
                 centered>
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter">
-                        Modal heading
+                        {pickedMentor.name}
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h4>Centered Modal</h4>
-                    <p>
-                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                        consectetur ac, vestibulum at eros.
-                    </p>
+                    <div className="modalBodyWrap">
+                        <div className="mentorB">
+                            <div className="mentorBL">
+                                <img src={image} alt="" />
+                                <h3>{pickedMentor.name}</h3>
+                                <h6>{pickedMentor.email}</h6>
+                                <h6>{pickedMentor.company}</h6>
+                            </div>
+                            <div className="mentorBR">
+                                <h4>멘토 소개 : </h4>
+                                <p>{pickedMentor.description}</p>
+                                <h4>경력 :</h4>
+                                {pickedMentor.career.map((career) => {
+                                        return (
+                                            <p key={career}>{career}</p>
+                                        )
+                                    })}
+                            </div>
+                        </div>
+                        <br/>
+                        <hr/>
+                        <div className="mentorApply">
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="멘토링 받고 싶은 내용을 입력하세요"
+                                multiline
+                                rows={4}
+                                variant="outlined"
+                                style={{width:'100%'}}
+                            />
+                            <Button variant="contained" className="applySubmit">신청하기</Button>
+                        </div>
+
+                    </div>
+
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={props.onHide}>Close</Button>
@@ -57,42 +104,45 @@ const MentorListB = () => {
 
     return (
         <>
-        <Grid container spacing={4} className="mentorListBW">
-            {mentorList.map((mentor) => {
-                return (
-                    <Grid key={mentor.usn} item xs={12} sm={4}>
-                        <Card className={classes.root}>
-                            <CardActionArea className="mentorCard">
-                                <CardMedia
-                                    className={classes.media}
-                                    image=""
-                                    title="mentorImage" />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        {mentor.name} 멘토
-                                    </Typography>
-                                    <Typography gutterBottom variant="body1" component="p">
-                                        {mentor.email}
-                                    </Typography>
-                                    <Typography gutterBottom variant="body1" component="p">
-                                        회사 : {mentor.company}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary" onClick={() => setModalShow(true)}>
-                                    자세히 보기
+            <Grid container spacing={4} className="mentorListBW">
+                {mentorList.map((mentor) => {
+                    return (
+                        <Grid key={mentor.usn} item xs={12} sm={4}>
+                            <Card className={classes.root}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        className={classes.media}
+                                        image={image}
+                                        title="mentorImage" />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            {mentor.name} 멘토
+                                        </Typography>
+                                        <Typography gutterBottom variant="body1" component="p">
+                                            {mentor.email}
+                                        </Typography>
+                                        <Typography gutterBottom variant="body1" component="p">
+                                            회사 : {mentor.company}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary" onClick={() => {
+                                        setModalShow(true);
+                                        setPickedMentor(mentor);
+                                    }} >
+                                        자세히 보기
                                 </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                )
-            })}
-        </Grid>
-        <MyVerticallyCenteredModal
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-        />
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
         </>
     );
 };
